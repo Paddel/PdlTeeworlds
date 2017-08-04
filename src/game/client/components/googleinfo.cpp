d1abe3e390a9239b7d76ddf470eeef36fb286b2f
Paddel <Patrick@weber-forchheim.de>
2017-08-04 19:42:25 +0200

    fixes

---
+++ src/game/client/components/googleinfo.cpp
@@ -0,0 +1,5 @@
+
+#include "googleinfo.h"
+
+#define SCHEMA_NAME "paddel"
+#define TABLE_NAME "identities"
---
+++ src/game/client/components/googleinfo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <engine/client/database.h>
+#include <game/client/component.h>
+
+class CGoogleInfo : public CComponent, public CDatabase
+{
+private:
+
+public:
+	//CGoogleInfo();
+
+	//static void CheckResult(int index, char *pResult, int pResultSize, void *pData);
+	//static void CheckGetIp(int index, char *pResult, int pResultSize, void *pData);
+};
